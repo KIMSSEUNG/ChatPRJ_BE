699400a40e06d8f6454571a58a1c343a8c8f17d8
KIMSSEUNG <tmdghdhkdw@gmail.com>
2025-02-24 20:43:16 +0900

    Merge로 인한 Message 도메인 에러 해결

--- src/main/java/com/hello/animalChat/domain/Message.java
+++ src/main/java/com/hello/animalChat/domain/Message.java
@@ -15,20 +15,13 @@ public class Message {
     @Column(name = "message_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-<<<<<<< HEAD
-    @JoinColumn(name = "member_id")
-    private Member sender;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-=======
     @JoinColumn(name = "sender_id")
     private Member sender;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id")
->>>>>>> ae50967bb6446f359b2a4d7dbcc6c4583e307d2d
     private Member receiver;
 
     @Column(columnDefinition = "TEXT")
@@ -37,13 +30,10 @@ public class Message {
     
     public Message(){}
 
-<<<<<<< HEAD
-    public Message(String message, LocalDateTime create_at) {
-=======
+
     public Message(Member sender , Member receiver ,  String message, LocalDateTime create_at) {
         this.sender =sender;
         this.receiver = receiver;
->>>>>>> ae50967bb6446f359b2a4d7dbcc6c4583e307d2d
         this.message = message;
         this.create_at = create_at;
     }
